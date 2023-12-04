67e400987c42f9158eccab88227c76623a3485ef
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-04 09:33:07 -0500

    Initial commit

---
+++ configuration.nix
@@ -0,0 +1,142 @@
+{ config, pkgs, ... }:
+
+let
+  home-manager = builtins.fetchTarball {
+    url = "https://github.com/nix-community/home-manager/archive/master.tar.gz";
+  };
+  
+in {
+  imports =
+    [
+      ./hardware-configuration.nix
+      "${home-manager}/nixos"
+    ];
+
+  # systemd-boot
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.configurationLimit = 20;
+  
+  # Hostname
+  networking.hostName = "pholi-nixos";
+
+  # wpa_supplicant
+  networking.wireless.enable = true;
+
+  # DHCP
+  networking.useDHCP = false;  # Set to false because it is deprecated
+  # We must set all interfaces individually
+  networking.interfaces.wlp4s0.useDHCP = true;
+  # Do the same for the ethernet
+  
+  # WiFi networks
+  networking.wireless.networks.AwesomenautsEXT.pskRaw = "6521e88582fdc0fda473fa548375627950a87185610768bed19eb41005409161";
+
+  # Date, time, locale
+  time.timeZone = "Canada/Eastern";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Users
+  users.mutableUsers = false;
+
+  users.users.root = {
+    isSystemUser = true;
+    hashedPassword = "$y$j9T$x96MzdskuJld7.MGfXLpE1$cOerhhSRfrLWEgyOI6vNmcRDnHshQ0e1QVDL3CMEFy3"; # asdf
+    # hashedPassword = "$y$j9T$G0Y8DMYeCovADHuKDb73a1$KqGTktIZlpdjEH4yubPnVTpZywk2Zf6fbk979YvKPk3";
+  };
+
+  # Home manager configuration for user jane.
+  home-manager.users.pholi = { pkgs, ... }: {
+    programs.home-manager.enable = true;
+    home.stateVersion = "23.05";
+  };
+  
+  users.users.pholi = {
+    isNormalUser = true;
+    home = "/home/pholi";
+    hashedPassword = "$y$j9T$x96MzdskuJld7.MGfXLpE1$cOerhhSRfrLWEgyOI6vNmcRDnHshQ0e1QVDL3CMEFy3"; # asdf
+    # hashedPassword = "$y$j9T$/agu8wY6h/PB20gbxj6aC.$JEuBPcl7F5crecpUFQ3SH.cEsNjMYD.8JnHArimSAt/";
+    description = "Philippe Olivier";
+    extraGroups = [ "wheel" "docker" ];
+    packages = with pkgs; [
+      # Desktop environment
+      alacritty
+      sway
+
+      # Misc
+      emacs
+      firefox
+    ];
+  };
+
+  programs.sway = {
+    enable = true;
+    # wrapperFeatures.gtk = true;
+  };
+  # environment.systemPackages = with pkgs; [
+  #   pkgs.powertop
+  #   pkgs.acpi
+  #   pkgs.upower
+  # ];
+  
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # users.users.alice = {
+  #   isNormalUser = true;
+  #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     tree
+  #   ];
+  # };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   wget
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  # system.stateVersion = "23.05"; # Did you read the comment?
+
+}
+
---
+++ nixos.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env -S bash -e
+
+set -e
+
+# To create the bootable USB image, download the latest unstable ISO here:
+# https://releases.nixos.org/nixos/unstable/nixos-24.05pre554114.e92039b55bcd
+# Then:
+# sudo dd if=nixos-minimal-....-x86_64-linux.iso of=/dev/sdX bs=4M status=progress conv=fdatasync
+#
+# To setup internet during installation:
+# wpa_passphrase SSID PASSWORD | sudo wpa_supplicant -B -i INTERFACE -c /dev/stdin
+#
+# To launch this script:
+# bash <(curl -sL URL_OF_THIS_SCRIPT)
+
+DEVICE="/dev/nvme0n1"
+RAM="$(free -m | sed -n '2p' | sed 's/[^0-9]*//' | cut -d' ' -f1)"
+BOOT_LABEL="BOOT"
+SWAP_LABEL="SWAP"
+ROOT_LABEL="NIXOS"
+BOOT_PARTITION="/dev/disk/by-partlabel/${BOOT_LABEL}"
+SWAP_PARTITION="/dev/disk/by-partlabel/${SWAP_LABEL}"
+ROOT_PARTITION="/dev/disk/by-partlabel/${ROOT_LABEL}"
+LUKS_MAPPING="cryptroot"
+CRYPTROOT="/dev/mapper/${LUKS_MAPPING}"
+
+# Delete the filesystem
+sudo wipefs -af "$DEVICE"
+
+# Delete the partition table
+sudo sgdisk -Zo "$DEVICE"
+
+# Partition the device (SWAP will be equal to the available RAM)
+sudo parted -s "$DEVICE" \
+     mklabel gpt \
+     mkpart $BOOT_LABEL fat32 1MiB 513MiB \
+     set 1 esp on \
+     mkpart $SWAP_LABEL linux-swap 513MiB $(( 513 + RAM ))MiB \
+     mkpart $ROOT_LABEL ext4 $(( 513 + RAM ))MiB 100%
+
+# Inform the kernel of changes
+sudo partprobe "$DEVICE"
+
+# Create the filesystems
+sudo mkfs.fat -F 32 -n $BOOT_LABEL $BOOT_PARTITION
+sudo mkswap -L $SWAP_LABEL $SWAP_PARTITION
+sudo swapon $SWAP_PARTITION
+read -r -s -p "Enter a password for the LUKS container: " LUKS_PASSPHRASE
+echo -n "$LUKS_PASSPHRASE" | sudo cryptsetup luksFormat --type luks2 "$ROOT_PARTITION" -d -
+echo -n "$LUKS_PASSPHRASE" | sudo cryptsetup open --type luks2 "$ROOT_PARTITION" "$LUKS_MAPPING" -d -
+sudo mkfs.ext4 -L $LUKS_MAPPING $CRYPTROOT
+
+# Mount partitions
+sudo mount $CRYPTROOT /mnt
+sudo mkdir -p /mnt/boot
+sudo mount $BOOT_PARTITION /mnt/boot
+
+# Generate `hardware-configuration.nix`
+sudo nixos-generate-config --root /mnt
+
+# Overwrite `configuration.nix` with my personal config file
+sudo curl -sLo /mnt/etc/nixos/configuration.nix pedtsr.ca/homelab/configuration.nix
+
+# Install NixOS
+sudo nixos-install --root /mnt --no-root-password

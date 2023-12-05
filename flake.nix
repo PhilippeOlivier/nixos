e4ec7666912d5290abddcd992ae58fc3ac57957c
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-05 11:52:37 -0500

    HM cleanup

--- flake.nix
+++ flake.nix
@@ -12,15 +12,10 @@
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
-        # make home-manager as a module of nixos
-        # so that home-manager configuration will be deployed automatically when executing `nixos-rebuild switch`
-        home-manager.nixosModules.home-manager
-        {
+        home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          # TODO replace ryan with your own username
-          home-manager.users.pholi = import ./home.nix;
-          # Optionally, use home-manager.extraSpecialArgs to pass arguments to home.nix
+          home-manager.users.pholi = import ./modules/home.nix;
         }
       ];
     };

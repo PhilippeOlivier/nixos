64ea75688d8900900b3ba7effdb2c2b8a404d593
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-05 11:43:13 -0500

    home manager test

---
+++ home.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+{
+  # TODO please change the username & home direcotry to your own
+  # home.username = "ryan";
+  # home.homeDirectory = "/home/ryan";
+
+  # link the configuration file in current directory to the specified location in home directory
+  # home.file.".config/i3/wallpaper.jpg".source = ./wallpaper.jpg;
+
+  # link all files in `./scripts` to `~/.config/i3/scripts`
+  # home.file.".config/i3/scripts" = {
+  #   source = ./scripts;
+  #   recursive = true;   # link recursively
+  #   executable = true;  # make all files executable
+  # };
+
+  # encode the file content in nix configuration file directly
+  # home.file.".xxx".text = ''
+  #     xxx
+  # '';
+
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    # here is some command line tools I use frequently
+    # feel free to add your own or remove some of them
+
+    neofetch
+
+  ];
+
+  # Let home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}

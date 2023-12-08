4bc3056495a93bd405fa2392dd6ae286d841c60a
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-08 14:52:05 -0500

    sway keybinds

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -4,12 +4,17 @@
     wrapperFeatures.gtk = true;
     config = rec {
       window = {
-        titlebar = false;
+        titlebar = false;  # Hide window title bars
       };
       terminal = "alacritty";
-      modifier = "Mod4";
+      modifier = "Mod4";  # The modifier key is the Super/Windows key
       keybindings = {
         "${modifier}+Return" = "exec ${terminal}";
+        "${modifier}+Shift+q" = "kill";  # Kill focused window
+        "${modifier}+Shift+c" = "reload";  # Reload configuration file
+        "${modifier}+Shift+e" = "exec swaymsg exit";  # Exit sway
+        "${modifier}+f" = "fullscreen";  # Make the current focus fullscreen
+
       };
     };
   };

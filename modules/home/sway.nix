657910e01d7d7eaff260f401ce3cf1e56e3aae52
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-14 14:21:13 -0500

    volume overhaul 4

--- modules/home/sway.nix
+++ modules/home/sway.nix
@@ -290,9 +290,9 @@
                 COLOR="#FFFF00"
             fi
 
-            echo "<span color="$COLOR"><b>Vol $OUTPUT</b></span>"
+            echo "<span color=\"$COLOR\"><b>Vol $OUTPUT</b></span>"
 
-            notify-send -h string:x-canonical-private-synchronous:anything -t 500 "VOLUME" "<span color=$COLOR font='50px'><b>$OUTPUT</b></span>"
+            notify-send -h string:x-canonical-private-synchronous:anything -t 500 "VOLUME" "<span color=\"$COLOR\" font='50px'><b>$OUTPUT</b></span>"
           '';
     	    interval = "once";
           tooltip = false;

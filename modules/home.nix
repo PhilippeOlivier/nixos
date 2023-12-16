0334e37f0294388e5d65096444bdb265dd6a479b
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-16 12:58:19 -0500

    bug 3

--- modules/home.nix
+++ modules/home.nix
@@ -6,10 +6,10 @@
     ./home/git.nix
     ./home/shell.nix
     ./home/sway.nix
-    # ./home/thunar.nix
-    # ./home/transmission.nix
+    ./home/thunar.nix
+    ./home/transmission.nix
     # ./home/xdg.nix
-    # ./home/zathura.nix
+    ./home/zathura.nix
   ];
 
   home = {

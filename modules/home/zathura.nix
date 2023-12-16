1872e94aae4e900973f671c7f08d89d00908103d
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-16 11:08:26 -0500

    zathura 4

--- modules/home/zathura.nix
+++ modules/home/zathura.nix
@@ -1,9 +1,9 @@
 { config, pkgs, ... }:
 
 {
-  home.packages = with pkgs; {
+  home.packages = with pkgs; [
     zathura
-  };
+  ];
   home.file.".config/zathura/zathurarc".text = ''
     set guioptions none
   '';

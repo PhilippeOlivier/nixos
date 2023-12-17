8334e9a846bfb635916654166353e77f4c4d9d57
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-17 13:07:59 -0500

    gnupg 3

--- modules/home/secrets.nix
+++ modules/home/secrets.nix
@@ -1,11 +1,17 @@
 { config, pkgs, ... }:
 
 {
+  
+  
   programs.gpg = {
     enable = true;
     homedir = "/home/pholi/nixos/secrets/gnupg";
   };
 
+  programs.gnupg.agent = {
+    enable = true;
+  };
+  
   services.gpg-agent = {
     enable = true;
     defaultCacheTtl = 8640000;

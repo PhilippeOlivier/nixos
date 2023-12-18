ee6ed24dd1763e7888bda1cd7673e674d6da14f5
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-18 16:33:59 -0500

    python 5

--- modules/python.nix
+++ modules/python.nix
@@ -2,6 +2,29 @@
 
 {
   environment.systemPackages = with pkgs; [
-    (python311.withPackages(ps: with ps; [ pandas requests]))
+    (python311.withPackages(ps: with ps; [
+      # General
+      pip
+
+      # Science
+      matplotlib
+      networkx
+      numpy
+      pandas
+      scipy
+
+      # Optimization
+      ortools
+
+      # Youtube download script
+      eyeD3
+      levenshtein
+      lz4
+      pyacoustid
+      youtube-dl
+
+      # Misc
+      grip
+    ]))
   ];
 }

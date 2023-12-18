90627146650dd3371c0438b3ad19ca499d2acf84
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-18 09:17:03 -0500

    sops-nix flakes

--- flake.nix
+++ flake.nix
@@ -5,9 +5,10 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
+  outputs = inputs @ { self, nixpkgs, home-manager, sops-nix, ... }: {
     nixosConfigurations.pholi-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -17,6 +18,7 @@
           home-manager.useUserPackages = true;
           home-manager.users.pholi = import ./modules/home.nix;
         }
+        sops-nix.nixosModules.sops
       ];
     };
   };

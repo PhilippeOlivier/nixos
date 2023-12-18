aa481f36f55ae00c69fe3b95060c2508ab13e4fd
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-18 16:23:12 -0500

    python 2

--- modules/python.nix
+++ modules/python.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ... }:
 
-{
-  let
-    my-python-packages = ps: with ps; [
-      matplotlib
-      pandas
-    ];
-  in
-    environment.systemPackages = [
-      (pkgs.python3.withPackages my-python-packages)
-    ];
-}
+let
+  my-python-packages = ps: with ps; [
+    matplotlib
+    pandas
+  ];
+in
+environment.systemPackages = [
+  (pkgs.python3.withPackages my-python-packages)
+];

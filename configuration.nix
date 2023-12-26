ba87790a481ad8ac2c6ba22b3e09c5e83f7591d4
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-26 12:58:19 -0500

    ssh secret 6

--- configuration.nix
+++ configuration.nix
@@ -19,11 +19,13 @@
 
   nixpkgs.config.allowUnfree = true;
 
+  imports = [ <sops-nix/modules/sops> ];
+  
   sops.defaultSopsFile = ./secrets/test.yaml;
   # # YAML is the default 
   sops.defaultSopsFormat = "yaml";
 
-  sops.secrets."ssh_key".mode = "0400";
+  # sops.secrets."ssh_key".mode = "0400";
   # sops.secrets.ssh_key = {
   #   format = "yaml";
   #   # can be also set per secret

160026103ee75a52c51af38e4ad20c21c1b8052d
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-26 13:06:35 -0500

    ssh secret 12

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
 
   nixpkgs.config.allowUnfree = true;
   
-  sops.defaultSopsFile = ./secrets.yaml;
+  sops.defaultSopsFile = "/home/pholi/nixos/secrets.yaml";
   # # YAML is the default 
   sops.defaultSopsFormat = "yaml";
 

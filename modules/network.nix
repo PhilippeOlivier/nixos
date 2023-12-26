b2806c294740d6ba89d72e094b86895545a89fd9
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-26 12:37:20 -0500

    added network again

--- modules/network.nix
+++ modules/network.nix
@@ -14,10 +14,8 @@
     interfaces.wlp4s0.useDHCP = true;
     interfaces.enp0s31f6.useDHCP = true;
   };
-
-
   
   # WiFi networks
-  #networking.wireless.networks.AwesomenautsEXT.pskRaw = "6521e88582fdc0fda473fa548375627950a87185610768bed19eb41005409161";
+  networking.wireless.networks.AwesomenautsEXT.pskRaw = "6521e88582fdc0fda473fa548375627950a87185610768bed19eb41005409161";
   # TODO: Other networks
 }

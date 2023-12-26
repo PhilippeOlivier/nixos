6e4530258f382b801772eb2c051db6e9874805d8
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-26 12:52:52 -0500

    ssh secret 2

--- modules/secrets.nix
+++ modules/secrets.nix
@@ -1,11 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  sops.defaultSopsFile = ../secrets/test.yaml;
+  sops.defaultSopsFile = ./secrets/test.yaml;
   # # YAML is the default 
   sops.defaultSopsFormat = "yaml";
 
-  sops.secrets."ssh_key".mode = "0400";
+  # sops.secrets."ssh_key".mode = "0400";
   # sops.secrets.ssh_key = {
   #   format = "yaml";
   #   # can be also set per secret

b3999b0af35785cb18116f76d5e46bcca65778d0
Philippe Olivier <philippe.olivier@polymtl.ca>
2023-12-28 19:22:03 -0500

    secrets 18

--- flake.nix
+++ flake.nix
@@ -18,9 +18,9 @@
           home-manager.useUserPackages = true;
           home-manager.users.pholi = import ./modules/home.nix;
         }
-        #sops-nix.homeManagerModules.sops
+        sops-nix.homeManagerModules.sops
         #sops-nix.nixosModules.sops
-        sops-nix/modules/home-manager/sops.nix
+        # sops-nix/modules/home-manager/sops.nix
       ];
     };
   };

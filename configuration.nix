d3f1ce91a64fb5c493bc50fc2fbd04067657541f
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-02 12:25:17 -0500

    boot

--- configuration.nix
+++ configuration.nix
@@ -10,7 +10,6 @@
     ./modules/python.nix
     ./modules/system.nix
     ./modules/users.nix
-    # ./secrets/wifi-networks.nix
   ];
 
   system.stateVersion = "24.05";
@@ -18,17 +17,5 @@
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   nixpkgs.config.allowUnfree = true;
-  
-  # sops.defaultSopsFile = ./secrets/secrets.yaml;
-  # # # YAML is the default 
-  # sops.defaultSopsFormat = "yaml";
-  # # sops.age.keyFile = "/home/pholi/.config/sops/age/keyz.txt";
-
-  # sops.secrets."ssh_key".mode = "0400";
-  # sops.secrets.ssh_key = {
-  #   format = "yaml";
-  #   # can be also set per secret
-  #   sopsFile = ./secrets/test.yaml;
-  # };
 }
 
--- flake.nix
+++ flake.nix
@@ -5,10 +5,9 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    sops-nix.url = "github:Mic92/sops-nix";
   };
 
-  outputs = inputs @ { self, nixpkgs, home-manager, sops-nix, ... }: {
+  outputs = inputs @ { self, nixpkgs, home-manager, ... }: {
     nixosConfigurations.pholi-nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
@@ -18,9 +17,6 @@
           home-manager.useUserPackages = true;
           home-manager.users.pholi = import ./modules/home.nix;
         }
-        #sops-nix.homeManagerModules.sops
-        sops-nix.nixosModules.sops
-        # sops-nix/modules/home-manager/sops.nix
       ];
     };
   };

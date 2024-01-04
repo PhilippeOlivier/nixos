1a2de0677c7a396be2a7ab13004e35b7ca191130
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-04 16:07:47 -0500

    python 3

--- modules/home/python.nix
+++ modules/home/python.nix
@@ -2,24 +2,46 @@
 
 {
   home = {
-    packages = with pkgs; [
-      (python311.withPackages(ps: with ps; [
-        # General
-        pip
-
-        # Science
-        matplotlib
-        networkx
-        numpy
-        pandas
-        scipy
-
-        # Optimization
-        ortools
-
-        # Misc
-        grip
-      ]))
-    ];
+    packages = with pkgs; (python311.withPackages(ps: with ps; [
+      # General
+      pip
+      
+      # Science
+      matplotlib
+      networkx
+      numpy
+      pandas
+      scipy
+      
+      # Optimization
+      ortools
+      
+      # Misc
+      grip
+    ]));
   };
 }
+
+#   {
+#   home = {
+#     packages = with pkgs; [
+#       (python311.withPackages(ps: with ps; [
+#         # General
+#         pip
+
+#         # Science
+#         matplotlib
+#         networkx
+#         numpy
+#         pandas
+#         scipy
+
+#         # Optimization
+#         ortools
+
+#         # Misc
+#         grip
+#       ]))
+#     ];
+#   };
+# }

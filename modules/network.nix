93eab89968712b0c7b188b1288eb2ee5aa67c474
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-05 16:26:36 -0500

    firewall

--- modules/network.nix
+++ modules/network.nix
@@ -11,5 +11,11 @@
     # Set all interfaces individually
     interfaces.wlp4s0.useDHCP = true;
     interfaces.enp0s31f6.useDHCP = true;
+
+    # Firewall
+    nftables.enable = true;  # Use the newer nftables instead of the older iptables
+    firewall = {
+      enable = true;
+    };
   };
 }

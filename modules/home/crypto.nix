5f21e0983d03b00df2925c29a200a746a3bdb2b3
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-07 12:44:45 -0500

    crypto 2

--- modules/home/crypto.nix
+++ modules/home/crypto.nix
@@ -24,7 +24,7 @@
     enable = true;
   };
 
-  home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/ssh";
+  # home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/ssh";
   
-  services.ssh-agent.enable = true;
+  # services.ssh-agent.enable = true;
 }

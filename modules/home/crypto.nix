12d0428fe8a87b38395e66e83986a764adc59ae6
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-07 12:52:06 -0500

    crypto 3

--- modules/home/crypto.nix
+++ modules/home/crypto.nix
@@ -5,24 +5,24 @@
     enable = true;
   };
   
-  home.file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
+  home.file."/home/pholi/.gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
 
-  services.gpg-agent = {
-    enable = true;
-    defaultCacheTtl = 8640000;
-    maxCacheTtl = 8640000;
-    pinentryFlavor = "curses";  # TODO: gtk2?
-  };
+  # services.gpg-agent = {
+  #   enable = true;
+  #   defaultCacheTtl = 8640000;
+  #   maxCacheTtl = 8640000;
+  #   pinentryFlavor = "curses";  # TODO: gtk2?
+  # };
 
-  programs.password-store = {
-    enable = true;
-  };
+  # programs.password-store = {
+  #   enable = true;
+  # };
 
-  home.file.".password-store".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/password-store";
+  # home.file.".password-store".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/password-store";
 
-  programs.ssh = {
-    enable = true;
-  };
+  # programs.ssh = {
+  #   enable = true;
+  # };
 
   # home.file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/ssh";
   

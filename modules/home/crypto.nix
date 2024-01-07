5c5766c6104137b62188ad14b2e4c545c32b400c
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-07 13:14:35 -0500

    crypto 10

--- modules/home/crypto.nix
+++ modules/home/crypto.nix
@@ -5,7 +5,7 @@
     enable = true;
   };
   
-  home.file.".gnupg".source = /home/pholi/.nixos-extra/gnupg;
+  home.file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
 
   # services.gpg-agent = {
   #   enable = true;

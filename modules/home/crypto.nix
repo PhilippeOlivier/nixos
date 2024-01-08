32cb40952b4cd17d52108bda3645eea29188b09d
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-08 15:08:08 -0500

    crypto 38

--- modules/home/crypto.nix
+++ modules/home/crypto.nix
@@ -1,27 +1,27 @@
 { config, pkgs, ... }:
 
 {
-
-  # services.gpg-agent = {
-  #   enable = true;
-  #   defaultCacheTtl = 8640000;
-  #   maxCacheTtl = 8640000;
-  #   pinentryFlavor = "curses";  # TODO: gtk2?
-  # };
-
-
   home = {
     packages = with pkgs; [
       gnupg
       openssh
       pass
+      pinentry
     ];
     file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
     file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/ssh";
     file.".password-store".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/password-store";
   };
-  
-  
-  
-  # services.ssh-agent.enable = true;
+
+  services = {
+    gpg-agent = {
+      enable = true;
+      defaultCacheTtl = 8640000;
+      maxCacheTtl = 8640000;
+      pinentryFlavor = "curses";  # TODO: gtk2?
+    };
+    ssh-agent = {
+      enable = true;
+    };
+  };
 }

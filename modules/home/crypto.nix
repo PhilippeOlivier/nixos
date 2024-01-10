d899ad7deb5fe5a22e5dde105b7fb2d44787fda9
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-10 10:14:27 -0500

    crypto 4

--- modules/home/crypto.nix
+++ modules/home/crypto.nix
@@ -3,13 +3,12 @@
 {
   home = {
     packages = with pkgs; [
-#      gnupg
+      gnupg
       openssh
       pass
       # pinentry
     ];
-    # file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
-    
+    file.".gnupg".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/gnupg";
     file.".ssh".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/ssh";
     file.".password-store".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nixos-extra/password-store";
   };

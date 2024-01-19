b3bf83f591a204b79f7527e5122de71b05046bb1
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-19 08:13:28 -0500

    custom kernel version

--- modules/boot.nix
+++ modules/boot.nix
@@ -2,4 +2,17 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 20;
+
+  # Choose custom kernel version
+  # Source: https://discourse.nixos.org/t/override-kernel-version/3638/5
+  boot.kernelPackages = pkgs.linuxPackagesFor (pkgs.linux_4_19.override {
+    argsOverride = rec {
+      src = pkgs.fetchurl {
+        url = "mirror://kernel/linux/kernel/v4.x/linux-${version}.tar.xz";
+        sha256 = "0ibayrvrnw2lw7si78vdqnr20mm1d3z0g6a0ykndvgn5vdax5x9a";
+      };
+      version = "4.19.60";
+      modDirVersion = "${version}";
+    };
+  });
 }

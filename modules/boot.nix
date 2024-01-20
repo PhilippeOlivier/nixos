c6d78a32b5b8c2638383491a9683ae6f02f49057
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-20 12:37:26 -0500

    kernel 4

--- modules/boot.nix
+++ modules/boot.nix
@@ -6,5 +6,5 @@
   boot.loader.systemd-boot.configurationLimit = 20;
 
   # Choose custom kernel version
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_6_5;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_latest;  # pkgs.linuxKernel.packages.linux_6_5
 }

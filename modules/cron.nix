26eddf746b3dbe18f367edc1398ecd31a7218aaf
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-30 14:51:30 -0500

    cron -> systemd test

--- modules/cron.nix
+++ modules/cron.nix
@@ -1,15 +1,39 @@
 { config, pkgs, ... }:
 
-{
-  services.cron = {
-    enable = true;
-    systemCronJobs = [
-      "*/1 * * * * pholi /home/pholi/.nixos-extra/scripts/cron/test.sh"
-    ];
-  };
-}
+# {
+#   services.cron = {
+#     enable = true;
+#     systemCronJobs = [
+#       "*/1 * * * * pholi /home/pholi/.nixos-extra/scripts/cron/test.sh"
+#     ];
+#   };
+# }
 
 # 0 3 * * * /home/pholi/.nixos-extra/scripts/cron/eyepatch.sh
 # */5 * * * * /home/pholi/.nixos-extra/scripts/mail/ntfy.sh
 # 0 * * * * /home/pholi/.nixos-extra/scripts/cron/hackernews.sh
 # 0 3 * * 1 /usr/bin/python /home/pholi/.nixos-extra/scripts/cron/alcohol.py
+
+#https://www.reddit.com/r/NixOS/comments/uc90q9/problems_with_crontab/
+
+{
+  systemd.services."test-service" = {
+    description = "My test service";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = /home/pholi/.nixos-extra/scripts/cron/test.sh; #"${pkgs.python3}/bin/python /home/eog/Documents/osu-history/screen_maker.py";
+      User = "pholi";
+      Group = "users";
+    };
+  };
+
+  systemd.timers."test-service" = {
+    description = "The timer for my test service";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnUnitActiveSec = "1m";
+      Unit = "test-service.service";
+    };
+  };
+}

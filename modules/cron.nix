921dc089e70c2eb23da5f30169c56b460bd35f9f
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-01-31 14:47:36 -0500

    cron -> systemd test 23

--- modules/cron.nix
+++ modules/cron.nix
@@ -36,26 +36,27 @@
   #   };
   # };
 
-  systemd.services."hn-service" = {
-    description = "HN jobs";
-    wantedBy = [ "multi-user.target" ];  # DELETE
-    path = [ pkgs.curl pkgs.jq pkgs.toybox ];
-    serviceConfig = {
-      Type = "oneshot";
-      ExecStart = "${pkgs.bash}/bin/bash /home/pholi/.nixos-extra/scripts/hn/hn.sh";
-      User = "pholi";
-      Group = "users";
-    };
-  };
+  # Still TODO, try on feb 1-2
+  # systemd.services."hn-service" = {
+  #   description = "HN jobs";
+  #   wantedBy = [ "multi-user.target" ];  # DELETE
+  #   path = [ pkgs.curl pkgs.jq pkgs.toybox ];
+  #   serviceConfig = {
+  #     Type = "oneshot";
+  #     ExecStart = "${pkgs.bash}/bin/bash /home/pholi/.nixos-extra/scripts/hn/hn.sh";
+  #     User = "pholi";
+  #     Group = "users";
+  #   };
+  # };
 
-  systemd.timers."hn-service" = {
-    description = "HN jobs timer";
-    wantedBy = [ "timers.target" ];
-    timerConfig = {
-      OnCalendar = "*-*-* *:38:00";
-      Unit = "hn-service.service";
-    };
-  };
+  # systemd.timers."hn-service" = {
+  #   description = "HN jobs timer";
+  #   wantedBy = [ "timers.target" ];
+  #   timerConfig = {
+  #     OnCalendar = "*-*-* *:38:00";
+  #     Unit = "hn-service.service";
+  #   };
+  # };
   
   # systemd.services."eyepatch-service" = {
   #   description = "Eyepatch";

7e8abf54d13f948c0ddc61756fb45fb43e64e3c2
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-04 13:51:28 -0500

    printers 4

--- modules/printers.nix
+++ modules/printers.nix
@@ -18,18 +18,18 @@
 
 # cups?
   
-  hardware.printers = {
-    ensurePrinters = [
-      {
-        name = "Brother_HL-3170CDW";
-        location = "Home";
-        deviceUri = "dnssd://Brother%20HL-3170CDW%20series._printer._tcp.local/?uuid=e3248000-80ce-11db-8000-30055ca8972a";
-        model = "Brother_HL-3170CDW_series.ppd";
-        ppdOptions = {
-          PageSize = "A4";
-        };
-      }
-    ];
-    ensureDefaultPrinter = "Brother_HL-3170CDW";
-  };
+  # hardware.printers = {
+  #   ensurePrinters = [
+  #     {
+  #       name = "Brother_HL-3170CDW";
+  #       location = "Home";
+  #       deviceUri = "dnssd://Brother%20HL-3170CDW%20series._printer._tcp.local/?uuid=e3248000-80ce-11db-8000-30055ca8972a";
+  #       model = "Brother_HL-3170CDW_series.ppd";
+  #       ppdOptions = {
+  #         PageSize = "A4";
+  #       };
+  #     }
+  #   ];
+  #   ensureDefaultPrinter = "Brother_HL-3170CDW";
+  # };
 }

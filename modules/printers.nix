0cf2a16c9d617665c490eb93059d0b7b9a3ae6aa
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-04 13:49:11 -0500

    printers 2

--- modules/printers.nix
+++ modules/printers.nix
@@ -12,22 +12,24 @@
 
   services.avahi = {
     enable = true;
-    nssmdns = true;
+    nssmdns4 = true;
     openFirewall = true;
   };
 
-  # hardware.printers = {
-  #   ensurePrinters = [
-  #     {
-  #       name = "HL-3170CDW";
-  #       location = "Home";
-  #       deviceUri = "http://192.168.178.2:631/printers/Dell_1250c";
-  #       model = "drv:///sample.drv/generic.ppd";
-  #       ppdOptions = {
-  #         PageSize = "A4";
-  #       };
-  #     }
-  #   ];
-  #   ensureDefaultPrinter = "HL-3170CDW";
-  # };
+# cups?
+  
+  hardware.printers = {
+    ensurePrinters = [
+      {
+        name = "Brother_HL-3170CDW";
+        location = "Home";
+        deviceUri = "dnssd://Brother%20HL-3170CDW%20series._printer._tcp.local/?uuid=e3248000-80ce-11db-8000-30055ca8972a";
+        model = "Brother_HL-3170CDW_series.ppd";
+        ppdOptions = {
+          PageSize = "A4";
+        };
+      }
+    ];
+    ensureDefaultPrinter = "Brother_HL-3170CDW";
+  };
 }

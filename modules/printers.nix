704fcd159dfa3dc1f6404871497f97de442801bb
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-05 10:35:02 -0500

    printers 9

--- modules/printers.nix
+++ modules/printers.nix
@@ -1,11 +1,12 @@
 { config, pkgs, ... }:
 
-let
-  brother_printer = pkgs.linkFarm "Brother_HL-3170CDW_series" [{
-    name = "share/cups/model/hl3170cdw.ppd";
-    path = "Brother_HL-3170CDW_series.ppd";
-  }];
-in
+# See: https://github.com/balsoft/nixos-config/blob/master/profiles/workspace/print-scan/default.nix
+# let
+#   brother_printer = pkgs.linkFarm "Brother_HL-3170CDW_series" [{
+#     name = "share/cups/model/hl3170cdw.ppd";
+#     path = "Brother_HL-3170CDW_series.ppd";
+#   }];
+# in
 
 {
   services.printing = {
@@ -14,8 +15,9 @@ in
       pkgs.brlaser
       pkgs.gutenprint
       pkgs.gutenprintBin
-      brother_printer
+      # brother_printer
     ];
+    cups-pdf.enable = true;
   };
 
   services.avahi = {
@@ -23,23 +25,4 @@ in
     nssmdns4 = true;
     openFirewall = true;
   };
-
-  # cups?
-  # do this: https://github.com/balsoft/nixos-config/blob/master/flake.nix
-# THIS:https://github.com/balsoft/nixos-config/blob/73cc2c3a8bb62a9c3980a16ae70b2e97af6e1abd/profiles/workspace/print-scan/default.nix#L4
-  
-  # hardware.printers = {
-  #   ensurePrinters = [
-  #     {
-  #       name = "Brother_HL-3170CDW";
-  #       location = "Home";
-  #       deviceUri = "dnssd://Brother%20HL-3170CDW%20series._printer._tcp.local/?uuid=e3248000-80ce-11db-8000-30055ca8972a";
-  #       model = "Brother_HL-3170CDW_series.ppd";
-  #       ppdOptions = {
-  #         PageSize = "A4";
-  #       };
-  #     }
-  #   ];
-  #   ensureDefaultPrinter = "Brother_HL-3170CDW";
-  # };
 }

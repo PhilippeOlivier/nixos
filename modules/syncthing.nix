1163a87a8cc2812cc4899fb839069e5a66b2b6d4
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-05 13:46:57 -0500

    syncthing 4

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -4,9 +4,23 @@
   services.syncthing = {
     enable = true;
     dataDir = "/home/pholi/syncthing";
-    configDir = "/home/pholi/testsyncconfig";
+    # configDir = "/home/pholi/testsyncconfig";
     openDefaultPorts = true;
     user = "pholi";
     group = "users";
+    overrideDevices = true;
+    overrideFolders = true;
+    settings = {
+      devices = {
+        # "device1" = { id = "DEVICE-ID-GOES-HERE"; };
+        # "device2" = { id = "DEVICE-ID-GOES-HERE"; };
+      };
+      folders = {
+        "Two-Way" = {
+          path = "/home/pholi/syncthing/twoway";
+          # devices = [ "device1" "device2" ];
+        };
+      };
+    };
   };
 }

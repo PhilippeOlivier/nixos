f81ed760114d52247774f6dc9e4e9c80620984e2
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-07 15:59:31 -0500

    symlink test 7

--- modules/home/zfs.nix
+++ modules/home/zfs.nix
@@ -2,9 +2,9 @@
 
 {
   # Symlinks for the `~/.zsnap` directory
-  home = {
-    file.".nixos-extra".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/.nixos-extra";
-    file."documents".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/documents";
-    file."syncthing".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/syncthing";
-  };
+  # home = {
+  #   file.".nixos-extra".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/.nixos-extra";
+  #   file."documents".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/documents";
+  #   file."syncthing".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.zsnap/syncthing";
+  # };
 }

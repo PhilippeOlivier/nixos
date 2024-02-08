ae762e29a72964216d0f3a4cc4d99c4a1136cb5f
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-08 09:44:49 -0500

    syncthing 4

--- modules/syncthing.nix
+++ modules/syncthing.nix
@@ -7,7 +7,7 @@
     enable = true;
     cert = "/home/pholi/.nixos-extra/syncthing/cert.pem";
     key = "/home/pholi/.nixos-extra/syncthing/key.pem";
-    dataDir = "/home/pholi/asdfsyncthing";
+    dataDir = "/home/pholi/asdf/syncthing"; #"/home/pholi/.nosnap/syncthing";
     openDefaultPorts = true;
     user = "pholi";
     group = "users";

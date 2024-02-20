fdffe10c1185144fd5943d450689c5dd91fd6e85
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-20 07:21:02 -0500

    sudo mount 5

--- modules/temp.nix
+++ modules/temp.nix
@@ -3,27 +3,29 @@
 {
   security.sudo = {
     enable = true;
-    extraRules = [{
-      users = [ "pholi" ];
-      commands = [
-        {
-          command = "${pkgs.systemd}/bin/systemctl suspend";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "${pkgs.systemd}/bin/reboot";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "${pkgs.systemd}/bin/poweroff";
-          options = [ "NOPASSWD" ];
-        }
-        {
-          command = "/run/wrappers/bin/mount";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-      groups = [ "wheel" ];
-    }];
+    extraRules = [
+      {
+        groups = [ "wheel" ];
+        # runAs = "root"; # <- ?
+        commands = [
+          {
+            command = "${pkgs.systemd}/bin/systemctl suspend";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/reboot";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "${pkgs.systemd}/bin/poweroff";
+            options = [ "NOPASSWD" ];
+          }
+          {
+            command = "/run/wrappers/bin/mount";  # MUST STILL USER sudo but not pw should be asked
+            options = [ "NOPASSWD" ];
+          }
+        ];
+      }
+    ];
   };
 }

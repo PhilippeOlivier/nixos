2320772a20aa4ce8ccbfdc377cb63f7197584e8b
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-20 07:55:11 -0500

    sudo mount 9

--- modules/temp.nix
+++ modules/temp.nix
@@ -9,35 +9,44 @@
         groups = [ "wheel" ];
         commands = [
           {
-            command = "/run/current-system/sw/bin/cryptsetup";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/current-system/sw/bin/zpool";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "${pkgs.coreutils}/bin/mkdir";  # TODO: change to homelab
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/wrappers/bin/mount";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "/run/wrappers/bin/umount";
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "${pkgs.coreutils}/bin/rmdir";  # TODO: change to homelab
-            options = [ "NOPASSWD" ];
-          }
-          {
-            command = "${pkgs.coreutils}/bin/udisksctl";  # TODO: change to homelab
+            command = "ALL";
             options = [ "NOPASSWD" ];
           }
         ];
       }
+      # {
+      #   groups = [ "wheel" ];
+      #   commands = [
+      #     {
+      #       command = "/run/current-system/sw/bin/cryptsetup";
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "/run/current-system/sw/bin/zpool";
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "${pkgs.coreutils}/bin/mkdir";  # TODO: change to homelab
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "/run/wrappers/bin/mount";
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "/run/wrappers/bin/umount";
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "${pkgs.coreutils}/bin/rmdir";  # TODO: change to homelab
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #     {
+      #       command = "${pkgs.coreutils}/bin/udisksctl";  # TODO: change to homelab
+      #       options = [ "NOPASSWD" ];
+      #     }
+      #   ];
+      # }
     ];
   };
 }

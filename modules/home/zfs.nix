3b540d04ce7e6f3bbc4513adb792b0fab3fb7036
Philippe Olivier <philippe.olivier@polymtl.ca>
2024-02-22 07:35:09 -0500

    some folders to nosnap

--- modules/home/zfs.nix
+++ modules/home/zfs.nix
@@ -3,6 +3,9 @@
 {
   # Symlinks for all directories that won't be snapshotted by ZFS
   home = {
+    file.".cache".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/cache";
+    file.".config".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/config";
+    file.".local".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/local";
     file."downloads".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/downloads";
     file."syncthing".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/syncthing";
     file."temp".source = config.lib.file.mkOutOfStoreSymlink "/home/pholi/.nosnap/temp";

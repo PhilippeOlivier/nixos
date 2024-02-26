a03f284861077aa708a97a7ab464e4640143e665
Philippe Olivier <philippe@pedtsr.ca>
2024-02-26 16:25:02 -0500

    imv xdg

--- modules/home.nix
+++ modules/home.nix
@@ -63,8 +63,8 @@
     shellAliases = {
       grip = ''grip --pass $(pass show github.com/token)'';  # Use my GitHub token to avoid the hourly rate limit
       ls = "ls --color=auto";  # Colorize the `ls` command
-      opto = "python ~/scripts/emma/opto/opto.py";  # Run the wife's opto script
-      # reboot = "emacsclient -e '(save-some-buffers t)' && reboot";  # Save all Emacs buffers before rebooting
+      # opto = "python ~/scripts/emma/opto/opto.py";  # Run the wife's opto script
+      reboot = "emacsclient -e '(save-some-buffers t)' && reboot";  # Save all Emacs buffers before rebooting
     };
   };
 

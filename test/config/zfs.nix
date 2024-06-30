53a03b1e39adad8d21d945d0f523dbb455e374cd
Philippe Olivier <philippe@pedtsr.ca>
2024-06-30 13:34:13 -0400

    sanoid interval 2

--- test/config/zfs.nix
+++ test/config/zfs.nix
@@ -20,7 +20,7 @@
   # Note: `syncoid` is managed manually by a separate script
   services.sanoid = {
     enable = true;
-    interval = "1m"; # <- temp (put "hourly")
+    interval = "hourly"; # <- temp (put "hourly")
     datasets = {
       "tank/snap" = {
         autoprune = true;

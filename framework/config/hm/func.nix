79a8541eb44717186bf800e7f13f89053b5260d6
Philippe Olivier <philippe@pedtsr.ca>
2024-07-03 12:28:35 -0400

    func works here 6

--- framework/config/hm/func.nix
+++ framework/config/hm/func.nix
@@ -18,16 +18,16 @@
 }:
 
 {
-  {
+  myfunc = {
     a
   }:
 
-  {
-    "/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
+    {
+      "/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
 [Desktop Entry]
 Name=${a}
 Type=Application
 Exec=${a}
     '';
-  };
+    };
 }

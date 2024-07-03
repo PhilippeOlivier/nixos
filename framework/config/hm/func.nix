e2f8bd9b716ab4a46a988ddbb84db961c6a2cf31
Philippe Olivier <philippe@pedtsr.ca>
2024-07-03 12:27:07 -0400

    func works here 4

--- framework/config/hm/func.nix
+++ framework/config/hm/func.nix
@@ -1,34 +1,33 @@
 # {
-#   functest = {
-#     a
-#   }:
+#   a
+# }:
 
-
-#     {
-#       xdg.dataFile."/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
+# {
+#    "/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
 # [Desktop Entry]
 # Name=${a}
 # Type=Application
 # Exec=${a}
 #     '';
-#     };
 # }
+
+
 
 {
   username
 }:
 
 {
-{
-  a
-}:
+  {
+    a
+  }:
 
-{
-   "/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
+  {
+    "/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
 [Desktop Entry]
 Name=${a}
 Type=Application
 Exec=${a}
     '';
-}
+  }
 }

a732f95b97a6b703ab703d7d4961c1b5aa2435c6
Philippe Olivier <philippe@pedtsr.ca>
2024-07-03 11:04:03 -0400

    func 11

--- framework/config/hm/func.nix
+++ framework/config/hm/func.nix
@@ -1,12 +1,15 @@
 {
-  a
 }:
 
 {
-  xdg.dataFile."/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
+  functest = {
+    a }:
+      {
+        xdg.dataFile."/home/pholi/.config/desktop-test/applications/${a}.desktop".text = ''
 [Desktop Entry]
 Name=${a}
 Type=Application
 Exec=${a}
     '';
+      };
 }

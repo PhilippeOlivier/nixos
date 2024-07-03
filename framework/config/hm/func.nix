24c757068e5a3d851eb3bd21960c8da9a0b43e5d
Philippe Olivier <philippe@pedtsr.ca>
2024-07-03 13:07:19 -0400

    launcher 2

--- framework/config/hm/func.nix
+++ framework/config/hm/func.nix
@@ -11,39 +11,3 @@ Type=Application
 Exec=${a}
 '';
 }
-
-
-
-# {
-#   username
-# }:
-
-# {
-#   myFunction = {
-#     a
-#   }:
-
-#     {
-#       "/home/${username}/.config/desktop-test/applications/${a}.desktop".text = ''
-# [Desktop Entry]
-# Name=${a}
-# # Type=Application
-# Exec=${a}
-# #     '';
-#     };
-# }
-
-# {
-#   username
-# }: a:
-
-
-# {
-#   "/home/${username}/.config/desktop-test/applications/${a}.desktop".text = ''
-# [Desktop Entry]
-# Name=${a}
-# Type=Application
-# Exec=${a}
-#      '';
-# }
-

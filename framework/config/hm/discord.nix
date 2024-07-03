19ab24e901ee6e4c28a8382a4ca176e16266fdcb
Philippe Olivier <philippe@pedtsr.ca>
2024-07-03 19:13:44 -0400

    hm 6

--- framework/config/hm/discord.nix
+++ framework/config/hm/discord.nix
@@ -9,17 +9,18 @@
     packages = with pkgs; [
       discord
     ];
-    # persistence."/nosnap/home/${username}" = {
-    #   directories = [
-    #     ".config/discord"
-    #   ];
-    # };
+    
+    persistence."/nosnap/home/${username}" = {
+      directories = [
+        ".config/discord"
+      ];
+    };
     
     # Prevent Discord from checking for new versions by itself
-    # file.".config/discord/settings.json".text = ''
-    #   {
-    #     "SKIP_HOST_UPDATE": true
-    #   }
-    # '';
+    file.".config/discord/settings.json".text = ''
+      {
+        "SKIP_HOST_UPDATE": true
+      }
+    '';
   };
 }

1d13efed7638df407f5a109b9643e0dfac3f5782
Philippe Olivier <philippe@pedtsr.ca>
2024-07-04 11:08:09 -0400

    func desktopDir test

--- framework/config/flake.nix
+++ framework/config/flake.nix
@@ -14,7 +14,7 @@
 
   outputs = inputs @ { self, home-manager, impermanence, nixpkgs, ... }:
     let
-      desktopEntriesDirectory = "${homeDirectory}/.config/pholi-desktop-entries/applications";  # TODO: remove homedir like for screenshots?
+      desktopEntriesDirectory = "${homeDirectory}/.config/pholi-desktop-entries/applications";
       homeDirectory = "/home/${username}";
       hostId = "cafe0000";
       hostName = "pholi-nixos";

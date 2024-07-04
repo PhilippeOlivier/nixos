b9d6986d155d6eb1606308a4f2d32d7e91bf8740
Philippe Olivier <philippe@pedtsr.ca>
2024-07-04 16:16:37 -0400

    discord

--- framework/config/hm/discord.nix
+++ framework/config/hm/discord.nix
@@ -1,26 +1,36 @@
 {
   pkgs
+, desktopEntriesDirectory
 , username
 , ...
 }:
+
+let
+  desktopEntry = import ./desktop-entry.nix { desktopEntriesDirectory = desktopEntriesDirectory; };
+in
 
 {
   home = {
     packages = with pkgs; [
       discord
     ];
-    
-    persistence."/nosnap/home/${username}" = {
-      directories = [
-        ".config/discord"
-      ];
-    };
-    
+
     # Prevent Discord from checking for new versions by itself
     file.".config/discord/settings.json".text = ''
       {
         "SKIP_HOST_UPDATE": true
       }
     '';
+
+    persistence."/nosnap/home/${username}" = {
+      directories = [
+        ".config/discord"
+      ];
+    };
+  };
+  
+  xdg.dataFile = desktopEntry {
+    name = "Discord";
+    exec = "discord";
   };
 }

9417a6489c45f34731186f4f4a7f6498d5534f10
Philippe Olivier <philippe@pedtsr.ca>
2024-07-05 17:15:41 -0400

    sway.nix and 2x crypto.nix

--- framework/config/nixos/default.nix
+++ framework/config/nixos/default.nix
@@ -8,12 +8,14 @@
     ./android.nix
     ./bluetooth.nix
     ./boot.nix
+    ./crypto.nix
     ./docker.nix
     ./fonts.nix
     #./framework.nix
     ./impermanence.nix
     ./mullvad.nix
     ./network.nix
+    ./sway.nix
     ./system.nix
     ./thunar.nix
     ./users.nix
@@ -22,7 +24,4 @@
 
 # TEMP: WiFi
   networking.wireless.networks.AwesomenautsEXT.pskRaw = "6521e88582fdc0fda473fa548375627950a87185610768bed19eb41005409161";
-
-  security.polkit.enable = true;  # Required for sway with HM
-  security.pam.services.swaylock = {};  # Required for swaylock to work
 }
---
+++ framework/config/nixos/sway.nix
@@ -0,0 +1,7 @@
+{
+  # Required for sway with HM
+  security.polkit.enable = true;
+
+  # Required for swaylock to work
+  security.pam.services.swaylock = {};
+}

1b184bcf1bb601796aaf1d8d7adc4adedad0b0e8
Philippe Olivier <philippe@pedtsr.ca>
2024-07-06 07:50:19 -0400

    all crypto

---
+++ framework/config/nixos/crypto.nix
@@ -0,0 +1,43 @@
+{
+  pkgs
+, username
+, ...
+}:
+
+{
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+      AuthenticationMethods = "publickey";
+      PrintMotd = false;
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    cryptsetup
+    gnupg
+    openssh
+    (pass.withExtensions (ext: with ext; [ pass-otp ]))
+    pinentry
+  ];
+
+  programs.gnupg.agent = {
+    enable = true;
+    settings = {
+      default-cache-ttl = 8640000;
+      max-cache-ttl = 8640000;
+    };
+    pinentryPackage = pkgs.pinentry-qt;
+  };
+
+  environment.persistence."/snap".users.${username} = {
+    directories = [
+      ".gnupg"
+      ".password-store"
+      ".ssh"
+    ];
+  };
+}

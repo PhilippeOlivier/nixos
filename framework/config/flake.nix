5d81eb8c3fc3d152f5729778458e8fc5bec4a92a
Philippe Olivier <philippe@pedtsr.ca>
2024-07-08 10:55:59 -0400

    sops age key location 6

--- framework/config/flake.nix
+++ framework/config/flake.nix
@@ -13,7 +13,7 @@
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = inputs @ { self, home-manager, impermanence, sops-nix, nixpkgs, ... }:
+  outputs = inputs @ { self, home-manager, impermanence, nixpkgs, ... }:
     let
       desktopEntriesDirectory = "${homeDirectory}/.config/pholi-desktop-entries";
       homeDirectory = "/home/${username}";
@@ -67,7 +67,6 @@
           modules = [
             ./configuration.nix
             impermanence.nixosModule
-            #sops-nix.nixosModules.sops
             home-manager.nixosModules.home-manager {
               home-manager.extraSpecialArgs = {
                 inherit

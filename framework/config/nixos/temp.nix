667f5bcaf5b4f363f24406f4f89cc6298bd0d6c0
Philippe Olivier <philippe@pedtsr.ca>
2024-07-08 11:46:00 -0400

    test script 6

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -1,7 +1,17 @@
-{
-pkgs, ...
-}:
-pkgs.writeShellScript "test-script-1.sh" ''
+let
+  pkgs = import <nixpkgs> {};
+  myScript = pkgs.writeShellScript "test-script-1.sh" ''
+    #!/bin/sh
     echo "This is the SOPS test script"
     echo "asdfasdf"
-  ''
+  '';
+in
+pkgs.stdenv.mkDerivation {
+  name = "my-script";
+  src = myScript;
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src $out/bin/my-script
+    chmod +x $out/bin/my-script
+  '';
+}

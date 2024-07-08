8f900c06dace52ff5cbf00bad69a85ebcda574c9
Philippe Olivier <philippe@pedtsr.ca>
2024-07-08 09:12:20 -0400

    removed HM sops

--- framework/config/flake.nix
+++ framework/config/flake.nix
@@ -107,7 +107,7 @@
               home-manager.users.${username}.imports = [
                 ./hm
                 (inputs.impermanence + "/home-manager.nix")
-                <sops-nix/modules/home-manager/sops.nix>
+                #<sops-nix/modules/home-manager/sops.nix>
               ];
             }
           ];

f28427f28b4dccc40a41ed4f718b9c8530a5a264
Philippe Olivier <philippe@pedtsr.ca>
2024-07-09 09:18:09 -0400

    sops HM works

--- framework/config/hm/sops.nix
+++ framework/config/hm/sops.nix
@@ -10,7 +10,9 @@
     defaultSopsFile = ../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
     secrets.mystring = {};
-    # https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules/
+
+    # Secrets will always be in that path
+    # Source: https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules
     defaultSymlinkPath = "/run/user/1000/secrets";
     defaultSecretsMountPoint = "/run/user/1000/secrets.d";
   };

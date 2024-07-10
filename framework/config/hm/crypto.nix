77c4b7461fecea1c9125295c5d6f25016ae6a3ef
Philippe Olivier <philippe@pedtsr.ca>
2024-07-10 09:27:11 -0400

    system secrets 11

--- framework/config/hm/crypto.nix
+++ framework/config/hm/crypto.nix
@@ -11,16 +11,16 @@ let
 in
 
 {
-  # sops = {
-  #   age.keyFile = sopsAgeKeyFilePath;
-  #   defaultSopsFile = ../secrets/secrets.yaml;
-  #   defaultSopsFormat = "yaml";
+  sops = {
+    age.keyFile = sopsAgeKeyFilePath;
+    defaultSopsFile = ../secrets/secrets.yaml;
+    defaultSopsFormat = "yaml";
 
-  #   # Secrets will always be in that path
-  #   # Source: https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules
-  #   defaultSymlinkPath = "/run/user/1000/secrets";
-  #   defaultSecretsMountPoint = "/run/user/1000/secrets.d";
-  # };
+    # Secrets will always be in that path
+    # Source: https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules
+    defaultSymlinkPath = "/run/user/1000/secrets";
+    defaultSecretsMountPoint = "/run/user/1000/secrets.d";
+  };
 
   home = {
     packages = with pkgs; [

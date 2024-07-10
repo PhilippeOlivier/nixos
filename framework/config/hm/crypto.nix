a350bb131f6769cc21abf79296e88fc25c3e1892
Philippe Olivier <philippe@pedtsr.ca>
2024-07-10 09:29:29 -0400

    system secrets 13

--- framework/config/hm/crypto.nix
+++ framework/config/hm/crypto.nix
@@ -15,6 +15,7 @@ in
     age.keyFile = sopsAgeKeyFilePath;
     defaultSopsFile = ../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
+    secrets.mystring - {};
 
     # Secrets will always be in that path
     # Source: https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules

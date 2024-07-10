a08752a5806c9e904557cd6383e564a6e2fae7f4
Philippe Olivier <philippe@pedtsr.ca>
2024-07-10 08:55:07 -0400

    secrets work

--- framework/config/hm/crypto.nix
+++ framework/config/hm/crypto.nix
@@ -20,8 +20,6 @@ in
     # Source: https://haseebmajid.dev/posts/2024-01-28-how-to-get-sops-nix-working-with-home-manager-modules
     defaultSymlinkPath = "/run/user/1000/secrets";
     defaultSecretsMountPoint = "/run/user/1000/secrets.d";
-
-    secrets.mystring = {};
   };
 
   home = {

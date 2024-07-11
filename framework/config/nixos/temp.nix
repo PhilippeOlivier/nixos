c2c406fb44f2983e05d452196246344efe3de33d
Philippe Olivier <philippe@pedtsr.ca>
2024-07-11 15:04:50 -0400

    systemd service test 4

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -1,14 +1,8 @@
 {pkgs, ...}:
 
 {
-  systemd.services."myuser@" = {
-    serviceConfig.ExecStart = 
-      let
-        script = pkgs.writeScript "myuser-start" ''
-        #!${pkgs.runtimeShell}
-        user="$1"
-        echo Hi, $user";
-      '';
-      in "${script} %u";
-  };
+  systemd.services."test" = {
+    script = ''
+      echo ASDF
+    '';
 }

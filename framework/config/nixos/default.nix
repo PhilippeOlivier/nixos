728bbd2c1cc156a4cc9c34293f654d3133509a17
Philippe Olivier <philippe@pedtsr.ca>
2024-07-11 09:54:13 -0400

    sops wireless cleanup

--- framework/config/nixos/default.nix
+++ framework/config/nixos/default.nix
@@ -21,18 +21,7 @@
     ./system.nix
     ./thunar.nix
     ./users.nix
+    ./wireless.nix
     ./zfs.nix
   ];
-
-  # TEMP: WiFi
-  #networking.wireless.networks.AwesomenautsEXT.pskRaw = "6521e88582fdc0fda473fa548375627950a87185610768bed19eb41005409161";
-
-
-  sops.secrets."wireless" = { };
-  networking.wireless.environmentFile = config.sops.secrets."wireless".path;
-  networking.wireless.networks = { "@home_ssid@" = { psk = "@home_psk@"; }; };
-
-
-  # networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
-  # networking.wireless.networks = { "@home_uuid@" = { psk = "@home_psk@"; }; };
 }
---
+++ framework/config/nixos/wireless.nix
@@ -0,0 +1,15 @@
+{
+  config
+, ...
+}:
+
+{
+  sops.secrets."wireless" = { };
+
+  networking.wireless = {
+    environmentFile = config.sops.secrets."wireless".path;
+    networks = {
+      "@home_ssid@".psk = "@home_psk@";
+    };
+  };
+}

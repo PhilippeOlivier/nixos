0fd50ee3faf278b4461040272e8c670124b62d10
Philippe Olivier <philippe@pedtsr.ca>
2024-07-11 09:37:34 -0400

    sops wifi 2

--- framework/config/nixos/default.nix
+++ framework/config/nixos/default.nix
@@ -29,7 +29,7 @@
 
   sops.secrets."wireless.env" = { };
 
-  networking.wireless.environmentFile = config.sops.secrets."wireless.env".path;
+  networking.wireless.environmentFile = sops.secrets."wireless.env".path;
   networking.wireless.networks = { "@home_uuid@" = { psk = "@home_psk@"; }; };
 
 

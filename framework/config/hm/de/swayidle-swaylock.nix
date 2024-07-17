6fa088f810864bdc56c02e60ba5d88566e48c3c5
Philippe Olivier <philippe@pedtsr.ca>
2024-07-17 13:54:11 -0400

    swaylock lid close 5

--- framework/config/hm/de/swayidle-swaylock.nix
+++ framework/config/hm/de/swayidle-swaylock.nix
@@ -45,15 +45,16 @@ in
   };
 
   systemd.user.services."swaylock-suspend" = {
-    description = "Lock screen before suspend";
-    before = [ "suspend.target" ];
-    wantedBy = [ "suspend.target" ];
-    script = "XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1 ${pkgs.swaylock}/bin/swaylock";
-    serviceConfig = {
-      Type = "forking";
-      User = username;
-      Group = "users";
+    Unit = {
+      Description = "Lock screen before suspend";
+      Requires = ["graphical-session.target"];
     };
+    Service = {
+      Type = "forking";
+    };
+    Script = "XDG_RUNTIME_DIR=/run/user/1000 WAYLAND_DISPLAY=wayland-1 ${pkgs.swaylock}/bin/swaylock";
+    Install.Before = [ "suspend.target" ];
+    Install.WantedBy = [ "suspend.target" ];
   };
 
   wayland.windowManager.sway.config.keybindings."${config.swayModifier}+l" = "exec ${lockScreenScript}";

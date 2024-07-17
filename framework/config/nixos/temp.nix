bf18ad5674e86f9cec919cc5c0fef62f3981f494
Philippe Olivier <philippe@pedtsr.ca>
2024-07-17 13:09:35 -0400

    systemd service touch 3

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -2,18 +2,18 @@
 
 {
   sops.secrets."ntfy_topic" = {};
-
+  # ${config.sops.secrets."ntfy_topic".path}
   systemd.services."test" = {
+    script = ''
+      touch /home/pholi/asdf
+    # '';
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = ''
-        touch /home/pholi/asdf
-      '';
+      # ExecStart = ''
+      #   touch /home/pholi/asdf
+      # '';
       User = "pholi";
       Group = "users";
     };
-    # script = ''
-    #   touch /home/pholi/asdf #${config.sops.secrets."ntfy_topic".path}
-    # '';
   };
 }

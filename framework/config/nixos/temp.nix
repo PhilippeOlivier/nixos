55dd59309c1e1081fcf4056f36e09183d3eb785b
Philippe Olivier <philippe@pedtsr.ca>
2024-07-17 13:43:31 -0400

    systemd service secret works

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -6,19 +6,13 @@
     group = config.users.users.pholi.group;
   };
 
-  # 
-        # touch /home/pholi/$(sudo cat /run/secrets/ntfy_topic)
   systemd.services."test" = {
     wantedBy = ["multi-user.target"];
     script = ''
       touch /home/pholi/$(cat ${config.sops.secrets."ntfy_topic".path})
     '';
-    # after = [ "sops-nix.service" ];
     serviceConfig = {
       Type = "oneshot";
-      # ExecStart = ''
-      #   touch /home/pholi/asdf
-      # '';
       User = "pholi";
       Group = "users";
     };

c05a9ea9e82fbafa1a5982580c7d150e8d384c20
Philippe Olivier <philippe@pedtsr.ca>
2024-07-17 13:29:51 -0400

    systemd service secret 8

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -3,10 +3,11 @@
 {
   sops.secrets."ntfy_topic" = {};
 
+      # touch /home/pholi/$(sudo cat ${config.sops.secrets."ntfy_topic".path})
   systemd.services."test" = {
     script = ''
       touch /home/pholi/asdf
-      touch /home/pholi/$(sudo cat ${config.sops.secrets."ntfy_topic".path})
+      touch /home/pholi/$(sudo cat /run/secrets/ntfy_topic)
       touch /home/pholi/ASDF
     '';
     # after = [ "sops-nix.service" ];

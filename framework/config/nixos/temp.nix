acbf08848917a03473a3122b26edea0a89cdeaa8
Philippe Olivier <philippe@pedtsr.ca>
2024-07-18 10:26:42 -0400

    mail systemd service test 22

--- framework/config/nixos/temp.nix
+++ framework/config/nixos/temp.nix
@@ -28,7 +28,7 @@
   # restart every time network comes online: https://unix.stackexchange.com/questions/725834/systemd-unit-auto-restart-when-network-changes
 
   systemd.network.enable = true;
-  network.useNetworkd = true;
+  networking.useNetworkd = true;
   
   services.networkd-dispatcher = {
     enable = true;

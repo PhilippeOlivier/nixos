a4ae1ae012b7731b1cf0dddf9a8ca6daf7edfc35
Philippe Olivier <philippe@pedtsr.ca>
2024-07-19 16:19:00 -0400

    systemd user mail works

--- framework/config/nixos/mail.nix
+++ framework/config/nixos/mail.nix
@@ -15,6 +15,7 @@
   services.networkd-dispatcher = {
     enable = true;
 
+    # this rule restarts the systemd "fetch-mail" system service
     rules."fetch-mail" = {
       onState = [ "routable" ];
       script = ''
@@ -24,7 +25,8 @@
     };
   };
 
-  systemd.services."fetch-mail" = {
+  # this system service restarts the systemd user service "fetch-mail"
+  systemd.services."fetch-mail" = {  # <- rename to fetch-mail-system and the other one to fetch-mail-user
     enable = true;
     script = ''
       ${pkgs.sudo}/bin/sudo systemctl --user -M pholi@ restart fetch-mail.service
@@ -34,8 +36,7 @@
       # User = "pholi";
       # Group = "users";
     };
-  };
-  
+  };  
   # systemd.services."fetch-mail" = {
   #   enable = true;
   #   wantedBy = [ "multi-user.target" ];

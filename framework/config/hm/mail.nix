7f1de72a24104eb0ea9914d1d90539cca3d876bc
Philippe Olivier <philippe@pedtsr.ca>
2024-07-20 12:47:23 -0400

    user mail script

--- framework/config/hm/mail.nix
+++ framework/config/hm/mail.nix
@@ -1,24 +1,55 @@
-{pkgs, ...}:
+{
+  pkgs
+, ...
+}:
 
-# let
-#   mail-fetch script...
-# in
+let
+  mail-fetch-script = "${pkgs.writeShellScriptBin "fetch-mail" ''
+          echo asdf
+        ''}/bin/fetch-mail";
+in
 
 
 {
   systemd.user.services."fetch-mail" = {
     Unit = {
-      Description = "ASFDASDFA";
+      Description = "Fetch mail for all mailboxes";
     };
-
     Install.WantedBy = [ "default.target" ];
-
     Service = {
       Type = "oneshot";
-      ExecStart = "${pkgs.writeShellScriptBin "fetch-mail" ''
-          echo asdf
-        ''}/bin/fetch-mail";
+      ExecStart = mail-fetch-script;
     };
   };
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# {
+#   systemd.user.services."fetch-mail" = {
+#     Unit = {
+#       Description = "ASFDASDFA";
+#     };
+
+#     Install.WantedBy = [ "default.target" ];
+
+#     Service = {
+#       Type = "oneshot";
+#       ExecStart = "${pkgs.writeShellScriptBin "fetch-mail" ''
+#           echo asdf
+#         ''}/bin/fetch-mail";
+#     };
+#   };
+
+# }

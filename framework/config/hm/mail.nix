5b125d8ef28b88a87459d1ccb34f5ba98a646908
Philippe Olivier <philippe@pedtsr.ca>
2024-07-24 08:59:28 -0400

    sops in mail script 9

--- framework/config/hm/mail.nix
+++ framework/config/hm/mail.nix
@@ -7,7 +7,7 @@
 
 let
   mail-fetch-script = "${pkgs.writeShellScriptBin "fetch-mail" ''
-    #echo $(${pkgs.coreutils}/bin/cat "/run/user/1000/secrets/words")
+    echo $(${pkgs.coreutils}/bin/cat "/run/user/1000/secrets/words")
     # words=("word1"
     #        "word2"
     #        "word3")
@@ -22,9 +22,7 @@ in
 {
   sops.secrets = {
     mystring = {};
-    words = {
-      file = /tmp/asdf;
-    }; #<-- is the array correct?
+    words = {};
   };
   
   systemd.user.services."fetch-mail" = {

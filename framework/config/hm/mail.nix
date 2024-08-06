d3515edb3bfc92b67ea0b2df7890523087520b83
Philippe Olivier <philippe@pedtsr.ca>
2024-08-06 10:04:59 -0400

    imper mail 20

--- framework/config/hm/mail.nix
+++ framework/config/hm/mail.nix
@@ -12,7 +12,7 @@
 let
   # This script is required instead of `services.mbsync.preExec` and `services.mbsync.postExec`
   # because we need the return code of the command `mbsync`
-  mail-fetch-script = "${pkgs.writeShellScriptBin "fetch-mail" ''
+  mail-fetch-script = "${pkgs.writeShellScriptBin "mail-fetch-script" ''
     #mkdir -p ${maildirsPath}/{${email1}}/{a,b,c}
     mkdir -p ${maildirsPath}/{a,b,c}
 
@@ -30,7 +30,7 @@ let
     done
 
    ${pkgs.curl}/bin/curl -d "New mail from: WOOO" ntfy.sh/"$(${pkgs.coreutils}/bin/cat "${config.sops.secrets.ntfyTopic.path}")"
-  ''}/bin/fetch-mail";
+  ''}/bin/mail-fetch-script";
 in
 
 {

2372adbff2816e1e6f73dc4b0365c1bf7e3432fc
Philippe Olivier <philippe@pedtsr.ca>
2024-08-06 14:48:57 -0400

    imper mail phase B 12

--- framework/config/hm/mail.nix
+++ framework/config/hm/mail.nix
@@ -54,7 +54,8 @@ let
             ${pkgs.gnused}/bin/sed -i "s/^''${mailbox}.*/''${mailbox},$(date +%s),''${error}/" $status_file
         fi
 
-
+        # Update the waybar mail module
+        ${pkgs.procps}/bin/pkill -RTMIN+${signalMail} waybar
     done
   ''}/bin/mail-fetch-scriptx2";
 in

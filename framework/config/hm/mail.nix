62e8173f0d92a09cb28f83c8540993dcff25d0ac
Philippe Olivier <philippe@pedtsr.ca>
2024-08-14 14:59:32 -0400

    mail debug 5

--- framework/config/hm/mail.nix
+++ framework/config/hm/mail.nix
@@ -85,7 +85,7 @@ let
                 ${pkgs.curl}/bin/curl -d "New mail from: $sender" ntfy.sh/"$(${pkgs.coreutils}/bin/cat "${config.sops.secrets.ntfyTopic.path}")"
                 thread_number="$(echo "$thread" | ${pkgs.gnused}/bin/sed -E 's/^thread:([0-9a-f]+).*$/\1/')"
                 echo thread number is $thread_number
-                touch "/tmp/''${thread_number}"
+                touch "/tmp/$thread_number"
             fi
         done <<< "$(${pkgs.notmuch}/bin/notmuch search tag:unread from:$sender)"
     done

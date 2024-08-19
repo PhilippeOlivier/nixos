4ec5a10875a5ccbf92733bd28aad29f14f027628
Philippe Olivier <philippe@pedtsr.ca>
2024-08-19 10:07:23 -0400

    eyepatch 21

--- framework/config/hm/scripts/eyepatch.nix
+++ framework/config/hm/scripts/eyepatch.nix
@@ -7,7 +7,7 @@
 }:
 
 let
-  eyepatch-script = pkgs.writeShellScriptBin "eyepatch-script4" ''
+  eyepatch-script = pkgs.writeShellScriptBin "eyepatch-script" ''
     track_file="${homeDirectory}/${eyepatchDirectory}/track"
     log_file="${homeDirectory}/${eyepatchDirectory}/log"
 

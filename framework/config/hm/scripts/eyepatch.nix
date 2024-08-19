efb11afd75b18cfb2f4b053e8d6b4e1e7b84a0f3
Philippe Olivier <philippe@pedtsr.ca>
2024-08-19 10:09:34 -0400

    eyepatch 22

--- framework/config/hm/scripts/eyepatch.nix
+++ framework/config/hm/scripts/eyepatch.nix
@@ -238,14 +238,12 @@ let
     init
 
     while read line; do
-        echo line=$line
         series=$(get_series_from_line "$line")
-        echo series=$series
-        # echo "Searching for new episodes for series: $(make_title "$series")"
-        # while series_has_new_episode "$series"; do
-        #     notify "$series"
-        #     ${pkgs.coreutils}/bin/sleep 5  # Make sure not to spam TPB so as to not get the IP banned
-        # done
+        echo "Searching for new episodes for series: $(make_title "$series")"
+        while series_has_new_episode "$series"; do
+            notify "$series"
+            ${pkgs.coreutils}/bin/sleep 5  # Make sure not to spam TPB so as to not get the IP banned
+        done
     done < "$track_file"
   '';
 in

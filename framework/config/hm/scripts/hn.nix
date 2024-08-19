4075dd5bca342ddacc5809c390c178ab27f467d3
Philippe Olivier <philippe@pedtsr.ca>
2024-08-19 16:01:20 -0400

    basic hn 5

--- framework/config/hm/scripts/hn.nix
+++ framework/config/hm/scripts/hn.nix
@@ -30,52 +30,52 @@ let
            "mathematical program")
 
     # Make sure the files exist
-    ${pkgs.coreutils}/bin/touch {"''${MATCH_FILE}","''${OLD_SUBMISSIONS}","''${JOBS_PROCESSED}"}
+    ${pkgs.coreutils}/bin/touch {"$MATCH_FILE","$OLD_SUBMISSIONS","$JOBS_PROCESSED"}
 
     # Check for any whoishiring (user) submissions that are not yet too old
     wih="$(${pkgs.curl}/bin/curl -sL https://hacker-news.firebaseio.com/v0/user/whoishiring.json)"
-    wih_submissions="$(echo "''${wih}" | ${pkgs.jq}/bin/jq ".submitted" | ${pkgs.jq}/bin/jq -r ".[]")"
+    wih_submissions="$(echo "$wih" | ${pkgs.jq}/bin/jq ".submitted" | ${pkgs.jq}/bin/jq -r ".[]")"
     active_submissions=()
     for submission in ''${wih_submissions}; do
         # If the submission is not yet marked as old
-        if ! ${pkgs.gnugrep}/bin/grep -qx "''${submission}" "''${OLD_SUBMISSIONS}"; then
+        if ! ${pkgs.gnugrep}/bin/grep -qx "$submission" "$OLD_SUBMISSIONS"; then
             # Check how old it is
             submission_time="$(${pkgs.curl}/bin/curl -sL "https://hacker-news.firebaseio.com/v0/item/''${submission}.json" | ${pkgs.jq}/bin/jq ".time")"
             current_time="$(${pkgs.coreutils}/bin/date +%s)"
             elapsed=$((current_time-submission_time))
             # If it is too old, add it to the old submissions, otherwise add it to the list
             if [[ elapsed -gt 1814400 ]]; then
-                echo "Submission ''${submission} is too old ($(${pkgs.coreutils}/bin/date -d "@''${submission_time}"))"
-                echo "''${submission}" >> "''${OLD_SUBMISSIONS}"
+                echo "Submission $submission is too old ($(${pkgs.coreutils}/bin/date -d "@$submission_time"))"
+                echo "$submission" >> "$OLD_SUBMISSIONS"
             else
-                active_submissions[''${#active_submissions[@]}]="''${submission}"
+                active_submissions[''${#active_submissions[@]}]="$submission"
             fi
         fi
     done
 
     # Process each active submission
-    for active_submission in "''${active_submissions[@]}"; do
+    for active_submission in "$active_submissions[@]"; do
         # Get the item ids of all the responses to that submission
         asub_json="$(${pkgs.curl}/bin/curl -sL "https://hacker-news.firebaseio.com/v0/item/''${active_submission}.json")"
-        responses="$(echo "''${asub_json}" | ${pkgs.jq}/bin/jq ".kids" | ${pkgs.jq}/bin/jq -r ".[]")"
-        for response in ''${responses}; do
-            if ${pkgs.gnugrep}/bin/grep -qx "''${response}" "''${JOBS_PROCESSED}"; then
-                echo "The response ''${response} has already been processed"
+        responses="$(echo "$asub_json" | ${pkgs.jq}/bin/jq ".kids" | ${pkgs.jq}/bin/jq -r ".[]")"
+        for response in $responses; do
+            if ${pkgs.gnugrep}/bin/grep -qx "$response" "$JOBS_PROCESSED"; then
+                echo "The response $response has already been processed"
             else
-                echo "Processing response ''${response}"
+                echo "Processing response $response"
                 response_json="$(${pkgs.curl}/bin/curl -sL "https://hacker-news.firebaseio.com/v0/item/''${response}.json")"
-                response_text="$(echo "''${response_json}" | ${pkgs.jq}/bin/jq ".text")"
+                response_text="$(echo "$response_json" | ${pkgs.jq}/bin/jq ".text")"
                 # Check if any search terms apply
                 for term in "''${TERMS[@]}"; do
                     if echo "$response_text" | ${pkgs.gnugrep}/bin/grep -is "$term"; then
-                        echo "Response ''${response} has a match!"
-                        url="https://news.ycombinator.com/item?id=''${response}"
-                        ${pkgs.curl}/bin/curl -d "[''${term}] ''${url}" ntfy.sh/pholi-homelab
-                        echo "[$(${pkgs.coreutils}/bin/date "+%Y-%m-%d %-H:%M")] [''${term}] ''${url}" >> "''${MATCH_FILE}"
+                        echo "Response $response has a match!"
+                        url="https://news.ycombinator.com/item?id=$response"
+                        ${pkgs.curl}/bin/curl -d "[$term] $url" ntfy.sh/pholi-homelab
+                        echo "[$(${pkgs.coreutils}/bin/date "+%Y-%m-%d %-H:%M")] [$term] $url" >> "$MATCH_FILE"
                         break
                     fi
                 done
-                echo "''${response}" >> "''${JOBS_PROCESSED}"
+                echo "$response" >> "$JOBS_PROCESSED"
             fi
         done
     done
